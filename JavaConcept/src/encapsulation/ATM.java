4ee818f039cb52651dd7808ad1b0594a3c9a7706
subha077 <46918101+subha077@users.noreply.github.com>
2019-05-22 14:03:19 +0530

    Commit on 22/05/2019 @02:03 P.M

---
+++ JavaConcept/src/encapsulation/ATM.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+public class ATM {
+
+	/*
+	 * Encapsulation--> Data binding,wrapping data and methods in a single unit.
+	 * Data Hiding --> Hiding the data from outside world, so that they can not be
+	 * accessed directly. 
+	 * Abstraction --> Force calling private variables through public methods.
+	 */
+
+	public static void main(String[] args) {
+
+		Bank obj = new Bank();
+		obj.updatePinNum(1478, 5897, 12589579);
+		obj.withdrawAmount(12589579, 5897, 10000);
+
+	}
+
+}
---
+++ JavaConcept/src/encapsulation/Bank.java
@@ -0,0 +1,45 @@
+package encapsulation;
+
+public class Bank {
+
+	public int accountNum = 12589579;
+	private int pinNum = 1478;
+	private double balanceAmount = 2000000;
+
+	public void withdrawAmount(int accNum, int pin, int amount) {
+		// TODO Auto-generated method stub
+
+		if (accNum == accountNum && pin == pinNum) {
+
+			System.out.println("Credentials matched!!");
+
+			if (amount < balanceAmount) {
+
+				balanceAmount = balanceAmount - amount;
+				System.out.println("The amount you have withdrawn: " + amount);
+				System.out.println("The balance amount in your account is: " + balanceAmount);
+
+			}
+
+			else {
+
+				System.out.println("Not enough balance in your account!!");
+			}
+
+		} else {
+			System.out.println("Invalid crdentials!!");
+		}
+	}
+
+	public void updatePinNum(int oldPin, int newPin, int accNum) {
+
+		if (accNum == accountNum && oldPin == pinNum) {
+
+			pinNum = newPin;
+			System.out.println("Pin changed successfully!!");
+		} else {
+			System.out.println("Invalid credentials.. Pin not changed!!");
+		}
+	}
+
+}
