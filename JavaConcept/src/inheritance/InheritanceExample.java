e8a1a0ce125b058c3c1c801c2fab2cba8c4c6347
subha077 <46918101+subha077@users.noreply.github.com>
2019-05-23 11:33:23 +0530

    Commit on 23/05/2019 @ 11:33 A.M

--- JavaConcept/src/inheritance/InheritanceExample.java
+++ JavaConcept/src/inheritance/InheritanceExample.java
@@ -1,5 +1,30 @@
 package inheritance;
 
 public class InheritanceExample {
-		/* Types of inheritance- Single Inheritance Multiple Inheritance */
+	
+		/* Types of inheritance- 
+		 * 
+		 * Single Inheritance, 
+		 * Multiple Inheritance, 
+		 * Multilevel Inheritance,
+		 * Hybrid Inheritance,
+		 * Hierarchical Inheritance
+		 * 
+		 * In java, inheritance is applied in two levels-
+		 * 
+		 * Class 
+		 * and
+		 * Interface
+		 * 
+		 * When a Class inherits a Class or 
+		 * an Interface inherits an Interface --> We use the 'extends' keyword.
+		 * 
+		 * When a Class inherits an Interface --> We use the 'implements' keyword.
+		 * 
+		 * At Class level the possible scenarios of Inheritance are-
+		 * Single Inheritance, 
+		 * Multilevel Inheritance,
+		 * Hierarchical Inheritance.
+		 * 
+		 */
 }
